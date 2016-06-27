2f06429d7ef626268252c564b52e35df94f16b5a
hsultova <hsultova@gmail.com>
2016-06-27 15:35:11 +0300

    Created controllers and views
    Add functionality of Home controller

--- jQuery and AJAX/05. Software-Technologies-Blog-Template/BlogWithCustomFramework/scripts/app.js
+++ jQuery and AJAX/05. Software-Technologies-Blog-Template/BlogWithCustomFramework/scripts/app.js
@@ -2,7 +2,7 @@
 	let baseUrl = "https://baas.kinvey.com";
 	let appKey = "kid_BJER_4RS";
 	let appSecret = "f61ad38ffd93438fb9c5ecebe6b1d34d";
-	var _guestCredentials = "Basic Z3Vlc3Q6Z3Vlc3Q=";
+	var _guestCredentials = "410b9462-b1d1-460c-9e11-5b431dacf517.mnov6Gb/yKTybDUuhtQeK9hnvYwW2kr9Y8Dy07zPif8=";
 
 	let selector = ".wrapper";
 	let mainContentSelector = ".main-content";
@@ -17,7 +17,8 @@
 	let requester = new Requester(authService);
 
 	let homeView = new HomeView(selector, mainContentSelector);
-	homeView.showGuestPage();
+	let homeController = new HomeController(homeView, requester, baseUrl, appKey);
+	homeController.showGuestPage();
 
 	initEventServices();
 
