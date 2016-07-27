5bcbb94f5044aa566a3383b6532d12520ff4f6f8
hsultova <hsultova@gmail.com>
2016-07-27 22:24:29 +0300

    Added MVC exercises

---
+++ Creating a Blog with ASP.NET MVC/MVCBlog/MVCBlog/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+using MVCBlog.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+
+[assembly: OwinStartupAttribute(typeof(MVCBlog.Startup))]
+namespace MVCBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+		}
+	}
+}
