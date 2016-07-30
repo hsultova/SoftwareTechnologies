10df6ab29de316dcc70d787eabfb2a88e522a187
hsultova <hsultova@gmail.com>
2016-07-31 00:07:11 +0300

    Added sidebar to all views

--- ASP.NET MVC Project/MessageBoard/MessageBoard/Controllers/HomeController.cs
+++ ASP.NET MVC Project/MessageBoard/MessageBoard/Controllers/HomeController.cs
@@ -1,16 +1,19 @@
-﻿using System;
+﻿using MessageBoard.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace MessageBoard.Controllers
 {
 	public class HomeController : Controller
 	{
+		private ApplicationDbContext db = new ApplicationDbContext();
 		public ActionResult Index()
 		{
-			return View();
+			return View(db.Categories.ToList());
 		}
 	}
 }
