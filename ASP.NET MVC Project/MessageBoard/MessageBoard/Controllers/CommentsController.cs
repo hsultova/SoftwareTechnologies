e0a9aca869a3bd03fdc8e464dde9987598ec9133
hsultova <hsultova@gmail.com>
2016-08-20 14:43:21 +0300

    Added Security - Users can change only their commentars

--- ASP.NET MVC Project/MessageBoard/MessageBoard/Controllers/CommentsController.cs
+++ ASP.NET MVC Project/MessageBoard/MessageBoard/Controllers/CommentsController.cs
@@ -9,137 +9,93 @@ using System.Web.Mvc;
 using MessageBoard.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using MessageBoard.Extensions;
 
 namespace MessageBoard.Controllers
 {
-    public class CommentsController : Controller
-    {
-        private ApplicationDbContext db = new ApplicationDbContext();
+	public class CommentsController : Controller
+	{
+		private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Comments
-        public ActionResult Index()
-        {
-            var comments = db.Comments.Include(c => c.Topic).Include(c => c.User);
-            return View(comments.ToList());
-        }
+		// GET: Comments/Edit/5
+		public ActionResult Edit(int? id)
+		{
 
-        // GET: Comments/Details/5
-        public ActionResult Details(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-			Comment comment = db.Comments.Include(t => t.User).Single(t => t.Id == id);
-
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Comment comment = db.Comments.Find(id);
 			if (comment == null)
-            {
-                return HttpNotFound();
-            }
-            return View(comment);
-        }
+			{
+				return HttpNotFound();
+			}
+			ViewBag.TopicId = new SelectList(db.Topics, "Id", "Title", comment.TopicId);
+			ViewBag.UserId = new SelectList(db.Users, "Id", "FullName", comment.UserId);
+			if (User.Identity.GetUserId() == comment.UserId)
+			{
+				return View(comment);
+			}
+			else
+			{
+				this.AddNotification("You don't have rights to change this comment.", NotificationType.INFO);
+				return RedirectToAction("Details", "Topics", new { id = comment.TopicId });
+			}
+		}
 
-        // GET: Comments/Create
-        public ActionResult Create()
-        {
-            ViewBag.TopicId = new SelectList(db.Topics, "Id", "Title");
-            ViewBag.UserId = new SelectList(db.Users, "Id", "FullName");
-            return View();
-        }
+		// POST: Comments/Edit/5
+		// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+		// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult Edit([Bind(Include = "Id,Text,DateCreated,UserId,TopicId")] Comment comment)
+		{
+			if (ModelState.IsValid)
+			{
+				db.Entry(comment).State = EntityState.Modified;
+				db.SaveChanges();
+				this.AddNotification("Comment edited.", NotificationType.SUCCESS);
+				return RedirectToAction("Details", "Topics", new { id = comment.TopicId });
+			}
+			ViewBag.TopicId = new SelectList(db.Topics, "Id", "Title", comment.TopicId);
+			ViewBag.UserId = new SelectList(db.Users, "Id", "FullName", comment.UserId);
+			return View(comment);
+		}
 
-        // POST: Comments/Create
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Id,Text,UserId,TopicId")] Comment comment/*, int id*/)
-        {
-            if (ModelState.IsValid)
-            {
-				comment.DateCreated = DateTime.Now;
-				UserManager<ApplicationUser> UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
-				ApplicationUser user = UserManager.FindById(User.Identity.GetUserId());
-				comment.User = user;
-				//comment.Topic= db.Topics.Include(t => t.Id == id);
+		// GET: Comments/Delete/5
+		public ActionResult Delete(int? id)
+		{
+			if (id == null)
+			{
+				return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+			}
+			Comment comment = db.Comments.Find(id);
+			if (comment == null)
+			{
+				return HttpNotFound();
+			}
+			return View(comment);
+		}
 
-				db.Comments.Add(comment);
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+		// POST: Comments/Delete/5
+		[HttpPost, ActionName("Delete")]
+		[ValidateAntiForgeryToken]
+		public ActionResult DeleteConfirmed(int id)
+		{
+			Comment comment = db.Comments.Find(id);
+			db.Comments.Remove(comment);
+			db.SaveChanges();
+			this.AddNotification("Comment deleted.", NotificationType.SUCCESS);
+			return RedirectToAction("Index");
+		}
 
-            ViewBag.TopicId = new SelectList(db.Topics, "Id", "Title", comment.TopicId);
-            ViewBag.UserId = new SelectList(db.Users, "Id", "FullName", comment.UserId);
-            return View(comment);
-        }
-
-        // GET: Comments/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Comment comment = db.Comments.Find(id);
-            if (comment == null)
-            {
-                return HttpNotFound();
-            }
-            ViewBag.TopicId = new SelectList(db.Topics, "Id", "Title", comment.TopicId);
-            ViewBag.UserId = new SelectList(db.Users, "Id", "FullName", comment.UserId);
-            return View(comment);
-        }
-
-        // POST: Comments/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Id,Text,DateCreated,UserId,TopicId")] Comment comment)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(comment).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.TopicId = new SelectList(db.Topics, "Id", "Title", comment.TopicId);
-            ViewBag.UserId = new SelectList(db.Users, "Id", "FullName", comment.UserId);
-            return View(comment);
-        }
-
-        // GET: Comments/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Comment comment = db.Comments.Find(id);
-            if (comment == null)
-            {
-                return HttpNotFound();
-            }
-            return View(comment);
-        }
-
-        // POST: Comments/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Comment comment = db.Comments.Find(id);
-            db.Comments.Remove(comment);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
-    }
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing)
+			{
+				db.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+	}
 }
