d75fe2a2ecf50b6119a05d6514bbef3e90b08673
hsultova <hsultova@gmail.com>
2016-08-29 21:21:47 +0300

    Added TopicsViewModel.cs

---
+++ ASP.NET MVC Project/MessageBoard/MessageBoard/ViewModels/TopicsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using MessageBoard.Helpers;
+using MessageBoard.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MessageBoard.ViewModels
+{
+	public class TopicsViewModel
+	{
+		public IEnumerable<Topic> Topics { get; set; }
+		public Pager Pager { get; set; }
+	}
+}
